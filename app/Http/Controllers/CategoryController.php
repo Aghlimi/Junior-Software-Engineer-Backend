1e2f0ca06ccba30d73a43cd4a310700d552e9b89
Aghlimi <ahmedghlimi123@gmail.com>
2026-04-24 23:18:44 +0100

    docs/style: add comment for CategoryController and format the file

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Services\CategoryService;
 
+/**
+ *  get all categories
+ */
 class CategoryController extends Controller
 {
-    public function __construct(private CategoryService $categoryService)
-    {}
+    public function __construct(private CategoryService $categoryService) {}
+
     /**
      * Handle the incoming request.
      */
